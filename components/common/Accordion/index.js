9666af022c7c194903ae389c039ad6d6ed2c257d
Parthiv-M <parthiv.menon@learner.manipal.edu>
2023-05-30 10:15:47 +0530

    Minor update to accordion component

--- components/common/Accordion/index.js
+++ components/common/Accordion/index.js
@@ -1,26 +1,26 @@
 import { useState, Fragment } from "react";
 
-export default function Accordion(props) {
-    const [isShowing, setIsShowing] = useState(false);
-
-    const toggle = () => {
-        setIsShowing(!isShowing);
-    };
-
-    const classDisplay = isShowing ? "block" : "hidden";
-    const fa_item = isShowing ? "fa-caret-down" : "fa-caret-up";
-
+const Accordion = (props) => {
+    const [isOpen, setIsOpen] = useState(false);
     return (
-        <div className="w-full rounded mb-2 cursor-pointer bg-[#ffffff39]">
-            <div className="bg-transparent w-full py-2 px-4 flex justify-between text-white select-none" onClick={toggle}>
-                <p className="text-lg font-normal">
-                    {props.title}
-                </p>
-                <i className={`fa ${fa_item} text-2xl`}></i>
+        <div className="w-full mt-5 text-white">
+            <div
+                className={`
+                    bg-white/10 flex justify-between items-center py-2 px-3 
+                    ${isOpen ? "rounded-tr-lg rounded-tl-lg" : "rounded-lg"}
+                `}
+            >
+                <h4 className="text-lg font-bold">{props.title}</h4>
+                <i
+                    className={`fa fa-chevron-${isOpen ? "up" : "down"} hover:cursor-pointer`}
+                    onClick={() => setIsOpen(!isOpen)}
+                ></i>
             </div>
-            <div className={`${classDisplay} px-3 py-2 bg-[#1b1b1b33] text-white`} >
+            <div className={`${isOpen ? "visible" : "hidden"} bg-white/5 p-2 rounded-br-lg rounded-bl-lg`}>
                 <Fragment>{props.content}</Fragment>
             </div>
         </div>
     );
 }
+
+export default Accordion
